7a786a4509a3c60cca0eac2a18173b892cc5cfbd
kanisanwin3 <nakashimakatsumi@yakisoba.local>
2014-06-15 05:09:58 +0900

    Initial Commit

---
+++ Lyrics/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lyrics
+//
+//  Created by 中島 克己 on 2014/06/15.
+//  Copyright (c) 2014年 katsumi nakashima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
